57a41cdcfb1e170abcfe177257d5699cdd9505ab
Adafede <adriano.rutz@unige.ch>
2021-09-22 15:46:44 +0200

    feat(environment): update dependencies and add missing R packages to environment.yml and environment_non_conda.sh

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -3,3 +3,7 @@
 echo "Installing packages missing from conda"
 
 R --slave -e "install.packages('readr', repo = \"https://cran.rstudio.com/\")"
+
+R --slave -e "install.packages('MSnbase', repo = \"https://bioconductor.org/packages/release/bioc/\")"
+
+R --slave -e "install.packages('nucleR', repo = \"https://bioconductor.org/packages/release/bioc/\")"
