7655a668632ea4c4857e9d76f411e2ffab597bfd
Adafede <adriano.rutz@unige.ch>
2021-12-31 08:47:19 +0100

    feat(plot_histograms.R): improve automatic peak detection and filtering

--- R/plot_histograms.R
+++ R/plot_histograms.R
@@ -9,35 +9,40 @@ require(package = ggplot2, quietly = TRUE)
 #' @export
 #'
 #' @examples
-plot_histograms <- function(dataframe, label, y = "values", xlab = TRUE) {
-  absolute <- ggplot2::ggplot(
-    dataframe,
-    ggplot2::aes(
-      x = sample,
-      y = get(y),
-      fill = ids
-    )
-  ) +
-    ggplot2::geom_col() +
-    ggplot2::geom_bar(stat = "identity") +
-    ggplot2::scale_fill_manual(
-      values = levels(dataframe$color) |>
-        as.character(),
-      guide = ggplot2::guide_legend(reverse = TRUE, ncol = 1)
+plot_histograms <-
+  function(dataframe,
+           label,
+           y = "values",
+           xlab = TRUE) {
+    absolute <- ggplot2::ggplot(
+      dataframe,
+      ggplot2::aes(
+        x = sample,
+        y = get(y),
+        fill = ids
+      )
     ) +
-    ggplot2::scale_x_discrete(labels = ifelse(test = xlab,
-      yes = levels(dataframe$sample),
-      no = ""
-    )) +
-    ggplot2::labs(fill = "Chemical class") +
-    ggplot2::theme_bw() +
-    ggplot2::theme(
-      legend.title = ggplot2::element_text(face = "bold"),
-      # legend.position = "none",
-      axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)
-    ) +
-    ggplot2::xlab(label) +
-    ggplot2::ylab("absolute")
+      ggplot2::geom_col() +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::scale_fill_manual(
+        values = levels(dataframe$color) |>
+          as.character(),
+        guide = ggplot2::guide_legend(reverse = TRUE, ncol = 1)
+      ) +
+      {
+        if (xlab == TRUE) {
+          ggplot2::scale_x_discrete(labels = levels(dataframe$sample))
+        }
+      } +
+      ggplot2::labs(fill = "Chemical class") +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        legend.title = ggplot2::element_text(face = "bold"),
+        # legend.position = "none",
+        axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)
+      ) +
+      ggplot2::xlab(label) +
+      ggplot2::ylab("absolute")
 
-  return(absolute)
-}
+    return(absolute)
+  }
