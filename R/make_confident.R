98346a282316dc0def41fc95fc71adb35675d934
Adafede <adriano.rutz@unige.ch>
2022-01-04 11:59:47 +0100

    update

---
+++ R/make_confident.R
@@ -0,0 +1,31 @@
+#' Title
+#'
+#' @param df
+#' @param score
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_confident <- function(df, score) {
+  df_ready <- df |>
+    dplyr::mutate(
+      best_candidate_1 = ifelse(
+        test = score_final >= score,
+        yes = best_candidate_1,
+        no = "notConfident"
+      ),
+      best_candidate_2 = ifelse(
+        test = score_final >= score,
+        yes = best_candidate_2,
+        no = "notConfident"
+      ),
+      best_candidate_3 = ifelse(
+        test = score_final >= score,
+        yes = best_candidate_3,
+        no = "notConfident"
+      )
+    )
+
+  return(df_ready)
+}
