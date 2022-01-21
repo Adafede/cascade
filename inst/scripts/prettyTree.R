9913b26f46eb752971968d15e762d3c902c992cc
Adafede <adriano.rutz@unige.ch>
2022-01-21 15:31:29 +0100

    feat(ui): update prettyTree.R script to change export name and filter levels

--- inst/scripts/prettyTree.R
+++ inst/scripts/prettyTree.R
@@ -23,13 +23,13 @@ source(file = "r/prepare_plot.R")
 classified_path <-
   "~/Git/lotus-processor/data/processed/211220_frozen_metadata.csv.gz"
 export_dir <- "~/Downloads"
-export_name <- "full"
+export_name <- "crazy"
 
-n_min <- 50
+n_min <- 100
 filter_level <- "organism_taxonomy_06family"
 filter_taxon <- NA #' replace with NA for no filter
-group_level <- "organism_taxonomy_06family"
-subgroup_level <- "organism_taxonomy_08genus"
+group_level <- "organism_taxonomy_08genus"
+subgroup_level <- "organism_taxonomy_09species"
 
 pairs_metadata <- readr::read_delim(file = classified_path) |>
   data.table::data.table()
@@ -51,8 +51,10 @@ taxon_restricted <- taxon_prerestricted |>
   dplyr::distinct(!!as.name(group_level))
 
 taxon_matched_restricted <-
-  rotl::tnrs_match_names(names = taxon_restricted[[1]],
-                         do_approximate_matching = FALSE)
+  rotl::tnrs_match_names(
+    names = taxon_restricted[[1]],
+    do_approximate_matching = FALSE
+  )
 
 ott_in_tree <-
   rotl::ott_id(taxon_matched_restricted)[rotl::is_in_tree(rotl::ott_id(taxon_matched_restricted))]
@@ -61,8 +63,10 @@ taxon_restricted <- taxon_restricted |>
   dplyr::filter(!!as.name(group_level) %in% names(ott_in_tree))
 
 taxon_matched_restricted <-
-  rotl::tnrs_match_names(names = taxon_restricted[[1]],
-                         do_approximate_matching = FALSE)
+  rotl::tnrs_match_names(
+    names = taxon_restricted[[1]],
+    do_approximate_matching = FALSE
+  )
 
 taxon_matched_restricted <- taxon_matched_restricted |>
   dplyr::mutate(key = paste(
@@ -80,8 +84,10 @@ tr_restricted <- rotl::tol_induced_subtree(ott_ids = ott_in_tree)
 
 specific_classes <- taxon_prerestricted |>
   splitstackshape::cSplit(
-    splitCols = colnames(pairs_metadata)[pairs_metadata[, grepl(pattern = "structure_taxonomy_npclassifier_",
-                                                                x = colnames(pairs_metadata))]],
+    splitCols = colnames(pairs_metadata)[pairs_metadata[, grepl(
+      pattern = "structure_taxonomy_npclassifier_",
+      x = colnames(pairs_metadata)
+    )]],
     sep = "|",
     direction = "long"
   ) |>
@@ -113,8 +119,9 @@ specific_classes <- taxon_prerestricted |>
   dplyr::filter(!!as.name(group_level) %in% taxon_matched_restricted$unique_name) |>
   dplyr::filter(!is.na(structure_taxonomy_npclassifier_03class)) |>
   dplyr::distinct(!!as.name(group_level),
-                  structure_inchikey,
-                  .keep_all = TRUE)
+    structure_inchikey,
+    .keep_all = TRUE
+  )
 
 specific_classes_o <- specific_classes |>
   dplyr::group_by(!!as.name(group_level)) |>
@@ -123,13 +130,17 @@ specific_classes_o <- specific_classes |>
   dplyr::ungroup()
 
 tr_restricted$tip.label <-
-  gsub(pattern = "_.*",
-       replacement = "",
-       x = tr_restricted$tip.label)
+  gsub(
+    pattern = "_.*",
+    replacement = "",
+    x = tr_restricted$tip.label
+  )
 
 taxonomy <-
-  dplyr::left_join(taxon_restricted,
-                   pairs_metadata) |>
+  dplyr::left_join(
+    taxon_restricted,
+    pairs_metadata
+  ) |>
   dplyr::distinct(
     Domain = organism_taxonomy_01domain,
     Kingdom = organism_taxonomy_02kingdom,
@@ -144,16 +155,18 @@ taxonomy <-
   )
 
 info <- taxonomy |>
-  dplyr::select(id = names(taxonomy)[grepl(
-    pattern = gsub(
-      pattern = "organism_taxonomy_[0-9]{2}",
-      replacement = "",
-      x = group_level
-    ),
-    x = names(taxonomy),
-    ignore.case = TRUE
-  )],
-  dplyr::everything()) |>
+  dplyr::select(
+    id = names(taxonomy)[grepl(
+      pattern = gsub(
+        pattern = "organism_taxonomy_[0-9]{2}",
+        replacement = "",
+        x = group_level
+      ),
+      x = names(taxonomy),
+      ignore.case = TRUE
+    )],
+    dplyr::everything()
+  ) |>
   dplyr::mutate(Kingdom = forcats::fct_reorder(Kingdom, !is.na(Domain))) |>
   dplyr::mutate(Phylum = forcats::fct_reorder(Phylum, !is.na(Kingdom))) |>
   dplyr::left_join(specific_classes_o, by = c("id" = group_level))
@@ -175,14 +188,16 @@ myPreplot <- prepare_plot(dataframe = myHierch)
 
 ott_ready <-
   ott_in_tree[!duplicated(ott_in_tree) &
-                names(ott_in_tree) %in% info$id]
+    names(ott_in_tree) %in% info$id]
 
 tr_ott <- rotl::tol_induced_subtree(ott_ids = ott_ready)
 
 tr_ott$tip.label <-
-  gsub(pattern = "_.*",
-       replacement = "",
-       x = tr_ott$tip.label)
+  gsub(
+    pattern = "_.*",
+    replacement = "",
+    x = tr_ott$tip.label
+  )
 
 tree_ott <- ggtree::ggtree(tr = tr_ott)
 
@@ -194,15 +209,19 @@ p_rel <- tree_ott %<+%
     size = ggplot2::rel(5),
     offset = ggplot2::rel(0.5)
   ) +
-  ggplot2::scale_color_manual(values = strsplit(x = paired, split = " "),
-                              na.value = "grey") +
+  ggplot2::scale_color_manual(
+    values = strsplit(x = paired, split = " "),
+    na.value = "grey"
+  ) +
   ggtreeExtra::geom_fruit(
     data = myPreplot,
     geom = geom_col,
     mapping =
-      ggplot2::aes(y = sample,
-                   x = relative,
-                   fill = ids),
+      ggplot2::aes(
+        y = sample,
+        x = relative,
+        fill = ids
+      ),
     offset = ggplot2::rel(0.15),
     pwidth = ggplot2::rel(1.2),
     orientation = "y",
@@ -224,14 +243,22 @@ p_rel <- tree_ott %<+%
   ) +
   ggtree::geom_tippoint(mapping = ggplot2::aes(color = Kingdom, size = o)) +
   ggnewscale::new_scale_fill() +
-  ggplot2::scale_fill_discrete(name = "Biological kingdom",
-                               guide = ggplot2::guide_legend(order = 1,
-                                                             ncol = 2)) +
-  ggplot2::scale_size_continuous(name = "Considered subtaxa in taxon",
-                                 guide = ggplot2::guide_legend(order = 2,
-                                                               direction = "horizontal")) +
+  ggplot2::scale_fill_discrete(
+    name = "Biological kingdom",
+    guide = ggplot2::guide_legend(
+      order = 1,
+      ncol = 2
+    )
+  ) +
+  ggplot2::scale_size_continuous(
+    name = "Considered subtaxa in taxon",
+    guide = ggplot2::guide_legend(
+      order = 2,
+      direction = "horizontal"
+    )
+  ) +
   ggplot2::theme(
-    legend.position = c(ggplot2::rel(0.15),  ggplot2::rel(0.93)),
+    legend.position = c(ggplot2::rel(0.15), ggplot2::rel(0.93)),
     legend.background = ggplot2::element_rect(fill = NA),
     legend.title = ggplot2::element_text(size = ggplot2::rel(3)),
     legend.text = ggplot2::element_text(size = ggplot2::rel(2)),
@@ -245,8 +272,10 @@ p_abs <- tree_ott %<+%
     size = ggplot2::rel(5),
     offset = ggplot2::rel(0.5)
   ) +
-  ggplot2::scale_color_manual(values = strsplit(x = paired, split = " "),
-                              na.value = "grey") +
+  ggplot2::scale_color_manual(
+    values = strsplit(x = paired, split = " "),
+    na.value = "grey"
+  ) +
   ggtreeExtra::geom_fruit(
     data = myPreplot |> dplyr::filter(parents == "Terpenoids"),
     geom = geom_col,
@@ -353,37 +382,45 @@ p_abs <- tree_ott %<+%
   ggplot2::scale_fill_manual(
     values = c(
       strsplit(rev(unique(
-        droplevels(myPreplot[myPreplot$parents == "Alkaloids",]$color)
+        droplevels(myPreplot[myPreplot$parents == "Alkaloids", ]$color)
       )) |> as.character(), split = " "),
       strsplit(rev(unique(
-        droplevels(myPreplot[myPreplot$parents == "Amino acids and Peptides",]$color)
+        droplevels(myPreplot[myPreplot$parents == "Amino acids and Peptides", ]$color)
       )) |> as.character(), split = " "),
       strsplit(rev(unique(
-        droplevels(myPreplot[myPreplot$parents == "Carbohydrates",]$color)
+        droplevels(myPreplot[myPreplot$parents == "Carbohydrates", ]$color)
       )) |> as.character(), split = " "),
       strsplit(rev(unique(
-        droplevels(myPreplot[myPreplot$parents == "Fatty acids",]$color)
+        droplevels(myPreplot[myPreplot$parents == "Fatty acids", ]$color)
       )) |> as.character(), split = " "),
       strsplit(rev(unique(
-        droplevels(myPreplot[myPreplot$parents == "Polyketides",]$color)
+        droplevels(myPreplot[myPreplot$parents == "Polyketides", ]$color)
       )) |> as.character(), split = " "),
       strsplit(rev(unique(
-        droplevels(myPreplot[myPreplot$parents == "Shikimates and Phenylpropanoids",]$color)
+        droplevels(myPreplot[myPreplot$parents == "Shikimates and Phenylpropanoids", ]$color)
       )) |> as.character(), split = " "),
       strsplit(rev(unique(
-        droplevels(myPreplot[myPreplot$parents == "Terpenoids",]$color)
+        droplevels(myPreplot[myPreplot$parents == "Terpenoids", ]$color)
       )) |> as.character(), split = " ")
     ),
     na.value = "grey"
   ) +
   ggtree::geom_tippoint(mapping = ggplot2::aes(color = Kingdom, size = o)) +
   ggnewscale::new_scale_fill() +
-  ggplot2::scale_fill_discrete(name = "Biological kingdom",
-                               guide = ggplot2::guide_legend(order = 1,
-                                                             ncol = 2)) +
-  ggplot2::scale_size_continuous(name = "Considered subtaxa in taxon",
-                                 guide = ggplot2::guide_legend(order = 2,
-                                                               direction = "horizontal")) +
+  ggplot2::scale_fill_discrete(
+    name = "Biological kingdom",
+    guide = ggplot2::guide_legend(
+      order = 1,
+      ncol = 2
+    )
+  ) +
+  ggplot2::scale_size_continuous(
+    name = "Considered subtaxa in taxon",
+    guide = ggplot2::guide_legend(
+      order = 2,
+      direction = "horizontal"
+    )
+  ) +
   ggplot2::theme(
     legend.position = c(ggplot2::rel(0.15), ggplot2::rel(0.93)),
     legend.background = ggplot2::element_rect(fill = NA),
@@ -392,7 +429,7 @@ p_abs <- tree_ott %<+%
   )
 
 ggplot2::ggsave(
-  filename =  file.path(
+  filename = file.path(
     export_dir,
     paste("tree", export_name, "relative.pdf", sep = "_")
   ),
