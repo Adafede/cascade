c931dbd4eda1fb3c46150a6c456e9b94fb3b27d6
Adafede <adriano.rutz@unige.ch>
2022-02-21 18:50:22 +0100

    feat(compare_peaks): improve peak detection and comparison functionality

--- R/extract_peak.R
+++ R/extract_peak.R
@@ -7,77 +7,36 @@
 #'
 #' @examples
 extract_peak <- function(peak) {
-  df <-
-    data.frame(
-      intensity = ms_chr[peak, 1]@intensity,
-      time = ms_chr[peak, 1]@rtime
-    ) |>
-    dplyr::filter(!is.na(intensity))
-
-  if (nrow(df) > 1) {
-    f <- approxfun(
-      x = df$time,
-      y = df$intensity
-    )
-
-    timeow <- seq(
-      from = min(df$time),
-      to = max(df$time),
-      by = (max(df$time) - min(df$time)) / 100
-    )
-
-    intensityeah <- f(seq(
-      from = min(df$time),
-      to = max(df$time),
-      by = (max(df$time) - min(df$time)) / 100
-    ))
-
-    df_extrapolated <-
-      data.frame(time = timeow, intensity = intensityeah)
-
-    # plot(df_extrapolated)
-    df_improved <-
-      improve_signal(
-        df = df_extrapolated,
-        time_min = min(df$time),
-        time_max = max(df$time)
-      )
-    # plot(df_improved)
-
-    f <- approxfun(
-      x = df_improved$time,
-      y = df_improved$intensity
-    )
-
-    timeow <- seq(
-      from = min(df$time),
-      to = max(df$time),
-      by = (max(df$time) - min(df$time)) / length(cad_ready$rtime)
-    )
-
-    intensityeah <- f(seq(
-      from = min(df$time),
-      to = max(df$time),
-      by = (max(df$time) - min(df$time)) / length(cad_ready$rtime)
-    ))
-
-    ms_ready <-
-      data.frame(rtime = timeow, intensity = intensityeah) |>
-      dplyr::filter(!is.na(intensity)) |>
-      dplyr::mutate(intensity = (intensity - min(intensity)) / (max(intensity) -
-        min(intensity))) |>
-      dplyr::filter(intensity >= 0.1) |>
-      dplyr::mutate(rtime = (rtime - min(rtime)) / (max(rtime) -
-        min(rtime))) |> #' see https://github.com/sneumann/xcms/issues/593
-      dplyr::arrange(rtime)
-
-    ms_peak <-
-      MSnbase::Chromatogram(
-        intensity = ms_ready$intensity,
-        rtime = ms_ready$rtime
-      )
-  } else {
-    ms_peak <- MSnbase::Chromatogram(intensity = 0, rtime = 0)
-  }
+  rtr <- df_peak[peak,] |>
+    dplyr::mutate(rtmin = (rt_min + CAD_SHIFT) * 60,
+                  rtmax = (rt_max + CAD_SHIFT) * 60) |>
+    dplyr::select(rtmin, rtmax) |>
+    as.matrix()
+  
+  mzr <- df_peak[peak,] |>
+    dplyr::select(mzmin = mz_min, mzmax = mz_max) |>
+    as.matrix()
+  
+  ms_chr <- dda_data_min |>
+    MSnbase::filterRt(c(rtr[1],rtr[2])) |> 
+    MSnbase::filterMz(c(mzr[1],mzr[2])) |> 
+    MSnbase::chromatogram(rt = rtr,
+                          mz = mzr)
+  
+  ms_ready <-
+    data.frame(intensity = ms_chr[1]@intensity,
+               rtime = ms_chr[1]@rtime) |>
+    dplyr::filter(!is.na(intensity)) |>
+    dplyr::mutate(intensity = (intensity - min(intensity)) / (max(intensity) -
+                                                                min(intensity))) |>
+    dplyr::filter(intensity >= 0.1) |>
+    dplyr::mutate(rtime = (rtime - min(rtime)) / (max(rtime) -
+                                                    min(rtime))) |> #' see https://github.com/sneumann/xcms/issues/593
+    dplyr::arrange(rtime)
+  
+  ms_peak <-
+    MSnbase::Chromatogram(intensity = ms_ready$intensity,
+                          rtime = ms_ready$rtime)
+  
   return(ms_peak)
 }
