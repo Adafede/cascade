a9eeba245327cebb6c597225ff77d5cc74dba42b
Adafede <adriano.rutz@unige.ch>
2022-02-25 11:58:02 +0100

    feat(chromatography): add filtering function for chromatography

---
+++ R/make_chromatographiable.R
@@ -0,0 +1,26 @@
+#' Title
+#'
+#' @param df
+#' @param mass_min
+#' @param mass_max
+#' @param logp_min
+#' @param logp_max
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_chromatographiable <-
+  function(df,
+           mass_min = 100,
+           mass_max = 1500,
+           logp_min = -1,
+           logp_max = 6) {
+    df_ready <- df |>
+      dplyr::filter(structure_exact_mass >= mass_min) |>
+      dplyr::filter(structure_exact_mass <= mass_max) |>
+      dplyr::filter(structure_xlogp >= logp_min) |>
+      dplyr::filter(structure_xlogp <= logp_max)
+    
+    return(df_ready)
+  }
