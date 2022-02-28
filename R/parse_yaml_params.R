a996a7eb9bf381d1bce5a9d27893a6c13ebd3610
Adafede <adriano.rutz@unige.ch>
2022-02-28 14:30:50 +0100

    feat(env): update environment.yml and environment_non_conda.sh to use default paths

--- R/parse_yaml_params.R
+++ R/parse_yaml_params.R
@@ -12,16 +12,24 @@ source(file = "R/log_debug.R")
 parse_yaml_params <- function() {
   log_debug("Loading yaml parameters")
   suppressWarnings(params <-
-    yaml::read_yaml(file = default_path, handlers = list(
-      seq = function(x) {
-        purrr::flatten(x)
-      }
-    )))
-  suppressWarnings(params <-
-    yaml::read_yaml(file = params_path, handlers = list(
-      seq = function(x) {
-        purrr::flatten(x)
-      }
-    )))
+    yaml::read_yaml(
+      file = paths$config$default$file,
+      handlers = list(
+        seq = function(x) {
+          purrr::flatten(x)
+        }
+      )
+    ))
+  if (file.exists(paths$config$params$file)) {
+    suppressWarnings(params <-
+      yaml::read_yaml(
+        file = paths$config$params$file,
+        handlers = list(
+          seq = function(x) {
+            purrr::flatten(x)
+          }
+        )
+      ))
+  }
   return(params)
 }
