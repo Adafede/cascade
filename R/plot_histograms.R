e1ed24ce59c825964083ad0b86123f7cdd25f4fe
Adafede <adriano.rutz@hotmail.com>
2022-05-18 10:46:07 +0200

    update

--- R/plot_histograms.R
+++ R/plot_histograms.R
@@ -4,6 +4,8 @@ require(package = ggplot2, quietly = TRUE)
 #'
 #' @param dataframe
 #' @param label
+#' @param y
+#' @param xlab
 #'
 #' @return
 #' @export
@@ -47,4 +49,86 @@ plot_histograms <-
       ggplot2::coord_flip()
 
     return(absolute)
+  }
+
+#' Title
+#'
+#' @param dataframe
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_histograms_confident <-
+  function(dataframe) {
+    plot <- ggplot2::ggplot(
+      dataframe,
+      ggplot2::aes(
+        x = peak_rt,
+        y = peak_area,
+        fill = name
+      )
+    ) +
+      ggplot2::geom_col() +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::scale_fill_manual(values = levels(dataframe$color) |>
+        as.character()) +
+      ggplot2::labs(fill = "Chemical Pathway - Superclass") +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        legend.title = ggplot2::element_text(face = "bold"),
+        # legend.title.align = 0.5,
+        # legend.position = "bottom",
+        # axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
+        panel.grid.major = element_blank(),
+        # panel.grid.minor = element_blank(),
+        # panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.text.y = ggplot2::element_text(face = "italic")
+      ) +
+      ggplot2::ylab("Area") +
+      ggplot2::xlab("Retention time [min]")
+
+    return(plot)
+  }
+
+#' Title
+#'
+#' @param dataframe
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_histograms_taxo <-
+  function(dataframe) {
+    plot <- ggplot2::ggplot(
+      dataframe,
+      ggplot2::aes(
+        x = peak_rt,
+        y = peak_area,
+        fill = name_2
+      )
+    ) +
+      ggplot2::geom_col() +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::scale_fill_manual(values = levels(dataframe$color_2) |>
+        as.character()) +
+      ggplot2::labs(fill = "Already reported in") +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        legend.title = ggplot2::element_text(face = "bold"),
+        # legend.title.align = 0.5,
+        # legend.position = "bottom",
+        # axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
+        panel.grid.major = element_blank(),
+        # panel.grid.minor = element_blank(),
+        # panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.text.y = ggplot2::element_text(face = "italic")
+      ) +
+      ggplot2::ylab("Area") +
+      ggplot2::xlab("Retention time [min]")
+
+    return(plot)
   }
