06ff39aa1eabca116195b17317b8034bb644ecd5
Adafede <adriano.rutz@hotmail.com>
2022-06-10 08:57:14 +0200

    feat(chromatograms): normalize_chromatograms_list function updated with new parameters and calculation

--- R/normalize_chromatograms_list.R
+++ R/normalize_chromatograms_list.R
@@ -1,7 +1,7 @@
 #' Title
 #'
-#' @param list 
-#' @param shift 
+#' @param list
+#' @param shift
 #'
 #' @return
 #' @export
@@ -9,8 +9,10 @@
 #' @examples
 normalize_chromatograms_list <- function(list, shift = 0) {
   df <- dplyr::bind_rows(list, .id = "id") |>
-    dplyr::mutate(intensity = intensity / max(intensity),
-                  time = time + shift) |>
+    dplyr::mutate(
+      intensity = intensity / max(intensity),
+      time = time + shift
+    ) |>
     dplyr::group_by(id) |>
     dplyr::mutate(time_2 = max(time)) |>
     dplyr::mutate(time = time / time_2)
