a3502ddfb4b9dac3d04b9b16e945fd35d86a12e5
Adafede <adriano.rutz@hotmail.com>
2022-07-26 21:00:18 +0200

    feat(plot_peaks_statistics): add test_full plotly visualization and treemaps progress script enhancements

--- R/treemaps_progress.R
+++ R/treemaps_progress.R
@@ -2,12 +2,13 @@
 #'
 #' @param xs
 #' @param type
+#' @param myWay
 #'
 #' @return
 #' @export
 #'
 #' @examples
-treemaps_progress <- function(xs, type = "treemap") {
+treemaps_progress <- function(xs, type = "treemap", myWay = FALSE) {
   p <- progressr::progressor(along = xs)
   future.apply::future_lapply(
     X = setNames(
@@ -16,25 +17,71 @@ treemaps_progress <- function(xs, type = "treemap") {
     ),
     FUN = function(x) {
       p()
-      if (x != "special") {
-        plotly::plot_ly(
-          data = hierarchies[[x]],
-          ids = ~ids,
-          labels = ~labels,
-          parents = ~parents,
-          values = ~values,
-          maxdepth = 3,
-          type = type,
-          branchvalues = "total",
-          textinfo = "label+percent value+percent parent+percent root"
-        ) |>
-          plotly::layout(
-            colorway = sunburst_colors,
-            title = paste(x, "(", nrow(
-              tables[[x]] |> dplyr::distinct(structure)
-            ), ")"),
-            margin = list(t = 40)
-          )
+      if (myWay == FALSE) {
+        if (x != "special") {
+          plotly::plot_ly(
+            data = hierarchies[[x]],
+            ids = ~ids,
+            labels = ~labels,
+            parents = ~parents,
+            values = ~values,
+            maxdepth = 3,
+            type = type,
+            branchvalues = "total",
+            textinfo = "label+percent value+percent parent+percent root"
+          ) |>
+            plotly::layout(
+              colorway = sunburst_colors,
+              title = paste(x, "(", nrow(
+                tables[[x]] |> dplyr::distinct(structure)
+              ), ")"),
+              margin = list(t = 40)
+            )
+        } else {
+          plotly::plot_ly() |>
+            plotly::add_trace(
+              data = hierarchies[[unique(hierarchies[[x]]$species)[1]]],
+              ids = ~ids,
+              labels = ~labels,
+              parents = ~parents,
+              values = ~values,
+              maxdepth = 3,
+              type = type,
+              branchvalues = "total",
+              textinfo = "label+percent value+percent parent+percent root",
+              domain = list(row = 0, column = 0)
+            ) |>
+            plotly::add_trace(
+              data = hierarchies[[unique(hierarchies[[x]]$species)[2]]],
+              ids = ~ids,
+              labels = ~labels,
+              parents = ~parents,
+              values = ~values,
+              maxdepth = 3,
+              type = type,
+              branchvalues = "total",
+              textinfo = "label+percent value+percent parent+percent root",
+              domain = list(row = 0, column = 1)
+            ) |>
+            plotly::layout(
+              title = paste(
+                "Comparative analysis",
+                "\n",
+                unique(hierarchies[[x]]$species)[1],
+                "(",
+                nrow(tables[[unique(hierarchies[[x]]$species)[1]]] |> dplyr::distinct(structure)),
+                ")",
+                "                                 ",
+                unique(hierarchies[[x]]$species)[2],
+                "(",
+                nrow(tables[[unique(hierarchies[[x]]$species)[2]]] |> dplyr::distinct(structure)),
+                ")"
+              ),
+              grid = list(rows = 1, columns = 2),
+              colorway = sunburst_colors,
+              margin = list(t = 40)
+            )
+        }
       } else {
         plotly::plot_ly() |>
           plotly::add_trace(
@@ -62,19 +109,6 @@ treemaps_progress <- function(xs, type = "treemap") {
             domain = list(row = 0, column = 1)
           ) |>
           plotly::layout(
-            title = paste(
-              "Comparative analysis",
-              "\n",
-              unique(hierarchies[[x]]$species)[1],
-              "(",
-              nrow(tables[[unique(hierarchies[[x]]$species)[1]]] |> dplyr::distinct(structure)),
-              ")",
-              "                                 ",
-              unique(hierarchies[[x]]$species)[2],
-              "(",
-              nrow(tables[[unique(hierarchies[[x]]$species)[2]]] |> dplyr::distinct(structure)),
-              ")"
-            ),
             grid = list(rows = 1, columns = 2),
             colorway = sunburst_colors,
             margin = list(t = 40)
