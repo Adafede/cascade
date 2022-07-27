2bb9ab57b2a2f41c6b677a3ea243fd7eebbf2bf2
Adafede <adriano.rutz@hotmail.com>
2022-07-27 16:17:15 +0200

    feat(ui): enhance plot_peaks_statistics function to include taxonomy data and display results with custom colors

--- R/add_peak_metadata.R
+++ R/add_peak_metadata.R
@@ -24,7 +24,8 @@ add_peak_metadata <- function(df) {
       score_chemical,
       score_final,
       #' add consensus
-      sample
+      sample,
+      keep
     ) |>
     dplyr::group_by(sample, peak_id) |>
     dplyr::distinct(feature_id,
@@ -87,7 +88,8 @@ add_peak_metadata <- function(df) {
       score_chemical,
       score_final,
       #' add consensus
-      sample
+      sample,
+      keep
     ) |>
     dplyr::group_by(sample, peak_id) |>
     dplyr::distinct(feature_id,
@@ -132,11 +134,96 @@ add_peak_metadata <- function(df) {
       correlatedChemicalPathwaysPerPeak
     )
 
+  df_meta_taxo <- df |>
+    dplyr::arrange(desc(feature_area)) |>
+    dplyr::filter(keep == "Y") |>
+    dplyr::filter(comparison_score >= params$chromato$peak$similarity$filter) |>
+    dplyr::distinct(
+      peak_id,
+      feature_id,
+      rt,
+      mz,
+      smiles_2D,
+      inchikey_2D,
+      molecular_formula,
+      best_candidate_1,
+      best_candidate_2,
+      best_candidate_3,
+      score_biological,
+      score_chemical,
+      score_final,
+      #' add consensus
+      sample,
+      keep
+    ) |>
+    dplyr::group_by(sample, peak_id) |>
+    dplyr::distinct(feature_id,
+      .keep_all = TRUE
+    ) |>
+    dplyr::add_count(name = "finalFeaturesPerPeak") |>
+    dplyr::distinct(molecular_formula,
+      smiles_2D,
+      inchikey_2D,
+      .keep_all = TRUE
+    ) |>
+    dplyr::add_count(name = "finalMolecularFormulasPerPeak") |>
+    dplyr::distinct(smiles_2D,
+      inchikey_2D,
+      .keep_all = TRUE
+    ) |>
+    dplyr::add_count(name = "finalStructuresPerPeak") |>
+    dplyr::distinct(best_candidate_1,
+      best_candidate_2,
+      best_candidate_3,
+      .keep_all = TRUE
+    ) |>
+    dplyr::add_count(name = "finalChemicalClassesPerPeak") |>
+    dplyr::distinct(best_candidate_1,
+      best_candidate_2,
+      .keep_all = TRUE
+    ) |>
+    dplyr::add_count(name = "finalChemicalSuperclassesPerPeak") |>
+    dplyr::distinct(best_candidate_1,
+      .keep_all = TRUE
+    ) |>
+    dplyr::add_count(name = "finalChemicalPathwaysPerPeak") |>
+    dplyr::ungroup() |>
+    dplyr::distinct(
+      sample,
+      peak_id,
+      finalFeaturesPerPeak,
+      finalMolecularFormulasPerPeak,
+      finalStructuresPerPeak,
+      finalChemicalClassesPerPeak,
+      finalChemicalSuperclassesPerPeak,
+      finalChemicalPathwaysPerPeak
+    )
+
+  my_bins <- function(x) {
+    case_when(
+      x == 0 ~ "0",
+      x == 1 ~ "01",
+      x > 1 & x <= 5 ~ "02-05",
+      x > 5 & x <= 10 ~ "06-10",
+      x > 10 ~ "10+"
+    )
+  }
+
   df_meta_full <- df_meta |>
     dplyr::full_join(df_meta_cor) |>
+    dplyr::full_join(df_meta_taxo) |>
     tidyr::fill(sample, .direction = "downup")
 
   df_meta_full[is.na(df_meta_full)] <- 0
+
+  df_meta_full <- df_meta_full |>
+    dplyr::mutate_at(
+      .vars = colnames(df_meta_full)[grepl(
+        pattern = "PerPeak",
+        x = colnames(df_meta_full)
+      )],
+      .funs = my_bins
+    )
 
   return(df_meta_full)
 }
--- R/plot_peaks_statistics.R
+++ R/plot_peaks_statistics.R
@@ -45,6 +45,25 @@ plot_peaks_statistics <- function(df) {
     dplyr::group_by(correlatedChemicalPathwaysPerPeak) |>
     dplyr::count()
 
+  test_features_tax <- df |>
+    dplyr::group_by(finalFeaturesPerPeak) |>
+    dplyr::count()
+  test_mf_tax <- df |>
+    dplyr::group_by(finalMolecularFormulasPerPeak) |>
+    dplyr::count()
+  test_structures_tax <- df |>
+    dplyr::group_by(finalStructuresPerPeak) |>
+    dplyr::count()
+  test_classes_tax <- df |>
+    dplyr::group_by(finalChemicalClassesPerPeak) |>
+    dplyr::count()
+  test_superclasses_tax <- df |>
+    dplyr::group_by(finalChemicalSuperclassesPerPeak) |>
+    dplyr::count()
+  test_pathways_tax <- df |>
+    dplyr::group_by(finalChemicalPathwaysPerPeak) |>
+    dplyr::count()
+
   test_full <- rbind(
     test_features,
     test_mf,
@@ -67,6 +86,21 @@ plot_peaks_statistics <- function(df) {
     test_classes_cor,
     test_superclasses_cor,
     test_pathways_cor
+  ) |>
+    dplyr::relocate(n, .before = 1) |>
+    tidyr::pivot_longer(2:7) |>
+    dplyr::filter(!is.na(value)) |>
+    dplyr::arrange(value) |>
+    dplyr::group_by(name) |>
+    dplyr::mutate(n = round(n / sum(n), 2))
+
+  test_full_tax <- rbind(
+    test_features_tax,
+    test_mf_tax,
+    test_structures_tax,
+    test_classes_tax,
+    test_superclasses_tax,
+    test_pathways_tax
   ) |>
     dplyr::relocate(n, .before = 1) |>
     tidyr::pivot_longer(2:7) |>
@@ -105,6 +139,30 @@ plot_peaks_statistics <- function(df) {
     )
   )
 
+  test_full_tax <- within(
+    test_full_tax,
+    name <- factor(
+      name,
+      levels = c(
+        "finalFeaturesPerPeak",
+        "finalMolecularFormulasPerPeak",
+        "finalStructuresPerPeak",
+        "finalChemicalClassesPerPeak",
+        "finalChemicalSuperclassesPerPeak",
+        "finalChemicalPathwaysPerPeak"
+      )
+    )
+  )
+
+  myDirtyColors <-
+    c(
+      "0" = "grey",
+      "01" = "#33a02c",
+      "02-05" = "#b2df8a",
+      "06-10" = "#ff7f00",
+      "10+" = "#fb9a99"
+    )
+
   bars_1 <- ggplot2::ggplot(
     data = test_full,
     ggplot2::aes(
@@ -120,7 +178,10 @@ plot_peaks_statistics <- function(df) {
       color = "white",
       position = ggplot2::position_stack(vjust = 0.5)
     ) +
-    scale_fill_viridis_c(option = "E") +
+    ggplot2::scale_colour_manual(
+      values = myDirtyColors,
+      aesthetics = c("colour", "fill")
+    ) +
     ggplot2::theme_minimal() +
     ggplot2::theme(
       # axis.text.x = element_blank(),
@@ -147,7 +208,10 @@ plot_peaks_statistics <- function(df) {
       color = "white",
       position = ggplot2::position_stack(vjust = 0.5)
     ) +
-    scale_fill_viridis_c(option = "E") +
+    ggplot2::scale_colour_manual(
+      values = myDirtyColors,
+      aesthetics = c("colour", "fill")
+    ) +
     ggplot2::theme_minimal() +
     ggplot2::theme(
       # axis.text.x = element_blank(),
@@ -159,11 +223,41 @@ plot_peaks_statistics <- function(df) {
       legend.position = "none"
     )
 
-  circles_1 <- plotly::plot_ly() |>
+  bars_3 <- ggplot2::ggplot(
+    data = test_full_tax,
+    ggplot2::aes(
+      x = name,
+      y = n,
+      fill = value,
+      cumulative = TRUE
+    )
+  ) +
+    ggplot2::geom_col() +
+    ggplot2::geom_text(
+      ggplot2::aes(label = paste(value, ":", 100 * n, "%")),
+      color = "white",
+      position = ggplot2::position_stack(vjust = 0.5)
+    ) +
+    ggplot2::scale_colour_manual(
+      values = myDirtyColors,
+      aesthetics = c("colour", "fill")
+    ) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      # axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks.x = element_blank(),
+      axis.ticks.y = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      legend.position = "none"
+    )
+
+  circles_1 <- plotly::plot_ly(colors = myDirtyColors) |>
     plotly::add_pie(
-      data = test_features_cor,
+      data = test_features_tax,
       name = "Features",
-      labels = ~correlatedFeaturesPerPeak,
+      labels = ~finalFeaturesPerPeak,
       values = ~n,
       sort = FALSE,
       type = "pie",
@@ -171,45 +265,45 @@ plot_peaks_statistics <- function(df) {
       domain = list(row = 0, column = 0)
     ) |>
     plotly::add_pie(
-      data = test_mf_cor,
+      data = test_mf_tax,
       name = "Molecular Formulas",
-      labels = ~correlatedMolecularFormulasPerPeak,
+      labels = ~finalMolecularFormulasPerPeak,
       values = ~n,
       type = "pie",
       textposition = "inside",
       domain = list(row = 0, column = 1)
     ) |>
     plotly::add_pie(
-      data = test_structures_cor,
+      data = test_structures_tax,
       name = "Structures",
-      labels = ~correlatedStructuresPerPeak,
+      labels = ~finalStructuresPerPeak,
       values = ~n,
       type = "pie",
       textposition = "inside",
       domain = list(row = 0, column = 2)
     ) |>
     plotly::add_pie(
-      data = test_classes_cor,
+      data = test_classes_tax,
       name = "Classes",
-      labels = ~correlatedChemicalClassesPerPeak,
+      labels = ~finalChemicalClassesPerPeak,
       values = ~n,
       type = "pie",
       textposition = "inside",
       domain = list(row = 1, column = 0)
     ) |>
     plotly::add_pie(
-      data = test_superclasses_cor,
+      data = test_superclasses_tax,
       name = "Superclasses",
-      labels = ~correlatedChemicalSuperclassesPerPeak,
+      labels = ~finalChemicalSuperclassesPerPeak,
       values = ~n,
       type = "pie",
       textposition = "inside",
       domain = list(row = 1, column = 1)
     ) |>
     plotly::add_pie(
-      data = test_pathways_cor,
+      data = test_pathways_tax,
       name = "Pathways",
-      labels = ~correlatedChemicalPathwaysPerPeak,
+      labels = ~finalChemicalPathwaysPerPeak,
       values = ~n,
       type = "pie",
       textposition = "inside",
@@ -217,26 +311,21 @@ plot_peaks_statistics <- function(df) {
     ) |>
     plotly::layout(
       title = "Peak analysis \n Features > Structures > Chemical classes > \n Superclasses > Pathways",
-      colorway = viridis::cividis(max(
-        nrow(test_features_cor),
-        nrow(test_mf_cor),
-        nrow(test_structures_cor),
-        nrow(test_classes_cor),
-        nrow(test_superclasses_cor),
-        nrow(test_pathways_cor)
-      )),
+      colorway = myDirtyColors,
       grid = list(rows = 2, columns = 3)
     )
 
   returned_list <- list(
     bars_1,
+    # circles_1,
     bars_2,
-    circles_1
+    bars_3
   )
   names(returned_list) <- c(
     "bars_1",
+    # circles_1,
     "bars_2",
-    "circles_1"
+    "bars_3"
   )
   return(returned_list)
 }
