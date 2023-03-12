59e6d1b70dd4316bff85a48b9f700d1e52b34cd0
Adriano Rutz <adriano.rutz@hotmail.com>
2023-03-12 06:55:47 +0100

    update

--- inst/scripts/0_generic_plot_chromatograms.R
+++ inst/scripts/0_generic_plot_chromatograms.R
@@ -20,6 +20,7 @@ source(file = "R/baseline_chromatogram.R")
 source(file = "R/baseline_chromatograms_progress.R")
 source(file = "R/change_intensity_name.R")
 source(file = "R/colors.R")
+# source(file = "R/dirty_paths_params.R")
 source(file = "R/get_params.R")
 source(file = "R/improve_signal.R")
 source(file = "R/improve_signals_progress.R")
@@ -27,8 +28,8 @@ source(file = "R/log_debug.R")
 source(file = "R/make_confident.R")
 source(file = "R/normalize_chromatograms_list.R")
 source(file = "R/parse_cli_params.R")
-source(file = "R/parse_yaml_params.R")
 source(file = "R/parse_yaml_paths.R")
+source(file = "R/parse_yaml_params.R")
 source(file = "R/peaks_progress.R")
 source(file = "R/plot_histograms.R")
 source(file = "R/prepare_hierarchy.R")
@@ -56,6 +57,9 @@ source(file = "R/dirty_paths_params.R")
 #' Specific paths
 TOYSET <- "inst/extdata/source/mzml/10043"
 TOYSET <- "inst/extdata/source/mzml/UHR"
+TOYSET <- "~/Downloads/bitter"
+TIME_MIN <- 0.3
+TIME_MAX <- 21.3
 
 files <- list.files(
   path = TOYSET,
@@ -104,10 +108,12 @@ names_2 <- list.files(
 objects <- lapply(files, mzR::openMSfile)
 objects_2 <- lapply(files_2, mzR::openMSfile)
 
-chromatograms <- lapply(objects, mzR::chromatograms)
+chromatograms <- mzR::chromatograms(object = objects[[
+  1
+]])
 chromatograms_2 <- lapply(objects_2, mzR::chromatograms)
 
-chromatograms_all <- purrr::flatten(chromatograms)
+chromatograms_all <- chromatograms
 chromatograms_all_2 <- purrr::flatten(chromatograms_2)
 
 chromatograms_bpi <- chromatograms_all[c(TRUE, FALSE, FALSE)]
@@ -119,19 +125,19 @@ chromatograms_cad <- chromatograms_all[c(FALSE, FALSE, TRUE)]
 chromatograms_pda <- chromatograms_all[c(FALSE, TRUE, FALSE)]
 
 chromatograms_bpi_ready <-
-  lapply(chromatograms_bpi, change_intensity_name, "BasePeak_0")
+  lapply(chromatograms_bpi, change_intensity_name, name = "BasePeak_0")
 
 chromatograms_bpi_neg_ready <-
-  lapply(chromatograms_bpi_neg, change_intensity_name, "BasePeak_0")
+  lapply(chromatograms_bpi_neg, change_intensity_name, name = "BasePeak_0")
 
 chromatograms_cad_ready <-
-  lapply(chromatograms_cad, change_intensity_name, "UV.1_CAD_1_0")
+  lapply(chromatograms_cad, change_intensity_name, name = "UV.1_CAD_1_0")
 
 chromatograms_pda_ready <-
   lapply(
     chromatograms_pda,
     change_intensity_name,
-    "PDA.1_TotalAbsorbance_0"
+    name = "PDA.1_TotalAbsorbance_0"
   )
 
 chromatograms_bpi_improved <-
@@ -143,10 +149,10 @@ chromatograms_cad_improved <-
 chromatograms_pda_improved <-
   improve_signals_progress(chromatograms_pda_ready)
 
-names(chromatograms_bpi_improved) <- names
-names(chromatograms_bpi_neg_improved) <- names_2
-names(chromatograms_cad_improved) <- names
-names(chromatograms_pda_improved) <- names
+# names(chromatograms_bpi_improved) <- names
+# names(chromatograms_bpi_neg_improved) <- names_2
+# names(chromatograms_cad_improved) <- names
+# names(chromatograms_pda_improved) <- names
 
 bpis_improved <- chromatograms_bpi_improved |>
   normalize_chromatograms_list()
@@ -293,53 +299,53 @@ new
 
 new_zoom <- plotly::plot_ly() |>
   plotly::add_lines(
-    data = cads_baselined |>
-      dplyr::filter(grepl(pattern = "10043", x = id)),
+    data = cads_improved |>
+      dplyr::filter(grepl(pattern = "1", x = id)),
     x = ~time,
     y = ~intensity,
     name = "<b> CAD </b>",
     line = list(
-      width = 3,
+      width = 1,
       # dash = "dot",
       color = "e31a1c"
     )
   ) |>
   plotly::add_lines(
-    data = pdas_baselined |>
-      dplyr::filter(grepl(pattern = "10043", x = id)),
+    data = pdas_improved |>
+      dplyr::filter(grepl(pattern = "1", x = id)),
     x = ~time,
     y = ~intensity,
     name = "<b> PDA </b>",
     line = list(
-      width = 3,
+      width = 1,
       # dash = "dot",
       color = "b2df8a"
     )
   ) |>
   plotly::add_lines(
-    data = bpis_baselined |>
-      dplyr::filter(grepl(pattern = "10043", x = id)),
+    data = bpis_improved |>
+      dplyr::filter(grepl(pattern = "1", x = id)),
     x = ~time,
     y = ~ -intensity,
     name = "<b> MS Pos </b>",
     line = list(
-      width = 3,
+      width = 1,
       # dash = "dot",
       color = "a6cee3"
     )
   ) |>
-  plotly::add_lines(
-    data = bpis_neg_baselined |>
-      dplyr::filter(grepl(pattern = "10043", x = id)),
-    x = ~time,
-    y = ~ -intensity,
-    name = "<b> MS Neg </b>",
-    line = list(
-      width = 3,
-      # dash = "dot",
-      color = "1f78b4"
-    )
-  ) |>
+  # plotly::add_lines(
+  #   data = bpis_neg_baselined |>
+  #     dplyr::filter(grepl(pattern = "10043", x = id)),
+  #   x = ~time,
+  #   y = ~ -intensity,
+  #   name = "<b> MS Neg </b>",
+  #   line = list(
+  #     width = 3,
+  #     # dash = "dot",
+  #     color = "1f78b4"
+  #   )
+  # ) |>
   plotly::layout(
     xaxis = list(title = "<b> Time [minutes] </b>"),
     yaxis = list(title = "<b> Normalized Intensity </b>")
@@ -354,6 +360,34 @@ new_zoom
 # reticulate::conda_install('r-reticulate', 'python-kaleido')
 # reticulate::conda_install('r-reticulate', 'plotly', channel = 'plotly')
 # reticulate::use_miniconda('r-reticulate')
+
+
+test <- plotly::plot_ly() |>
+  plotly::add_lines(
+    data = cads_improved |>
+      dplyr::filter(grepl(pattern = "1", x = id)),
+    x = ~ chromatograms_cad_ready[[1]]$time,
+    y = ~ chromatograms_cad_ready[[1]]$intensity,
+    name = "<b> CAD </b>",
+    line = list(
+      width = 1,
+      # dash = "dot",
+      color = "e31a1c"
+    )
+  ) |>
+  plotly::add_lines(
+    data = pdas_improved |>
+      dplyr::filter(grepl(pattern = "1", x = id)),
+    x = ~ chromatograms_cad_improved[[1]]$time,
+    y = ~ chromatograms_cad_improved[[1]]$intensity,
+    name = "<b> PDA </b>",
+    line = list(
+      width = 1,
+      # dash = "dot",
+      color = "b2df8a"
+    )
+  )
+test
 
 plotly::save_image(
   p = new,
