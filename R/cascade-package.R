ab6b14885ad196f32fc3bd0f9f35a0d0c6a72577
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-01 11:51:32 +0100

    refactor(package): improve package documentation and internal processing

--- R/cascade-package.R
+++ R/cascade-package.R
@@ -7,23 +7,23 @@ NULL
 .datatable.aware <- TRUE
 
 .onLoad <- function(libname, pkgname) {
-  options("pboptions" = list(
-    type = if (interactive()) "timer" else "none",
-    char = "-",
-    txt.width = 50,
-    gui.width = 300,
-    style = 3,
-    initial = 0,
-    title = "R progress bar",
-    label = "",
-    nout = 100L
-  ))
+  ## Conflicts with chromatographR
+  # options("pboptions" = list(
+  #   type = if (interactive()) "timer" else "none",
+  #   char = "-",
+  #   txt.width = 50,
+  #   gui.width = 300,
+  #   style = 3,
+  #   initial = 0,
+  #   title = "R progress bar",
+  #   label = "",
+  #   nout = 100L
+  # ))
   strat <- ifelse(test = .Platform$OS.type == "unix",
     yes = "multicore",
     no = "multisession"
   )
   future::plan(strategy = strat, workers = future::nbrOfWorkers())
-  # handlers(global = TRUE)
   progressr::handlers(
     progressr::handler_txtprogressbar(enable = TRUE),
     progressr::handler_progress(format = ":spin [:bar] ETA: :eta :percent")
--- R/peaks_progress.R
+++ R/peaks_progress.R
@@ -38,8 +38,7 @@ peaks_progress <- function(xs) {
       names(data_cascade)[[1]] <- "666"
 
       data <-
-        # chromatographR::preprocess(
-        preprocess(
+        chromatographR::preprocess(
           X = data_cascade,
           dim1 = seq(
             from = as.numeric(rownames(data_cascade[[1]])[[1]]),
@@ -54,8 +53,7 @@ peaks_progress <- function(xs) {
           spec.smooth = FALSE
         )
 
-      # pks <- chromatographR::get_peaks(
-      pks <- get_peaks(
+      pks <- chromatographR::get_peaks(
         chrom_list = data,
         lambdas = c("666"),
         # fit = c("egh", "gaussian", "raw"),
