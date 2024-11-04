4892c22f14e71392cd2255d3858b55fe6f08a78e
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-04 16:17:40 +0100

    feat(auth): update dependencies to latest versions and remove unused packages

--- R/cascade-package.R
+++ R/cascade-package.R
@@ -7,27 +7,12 @@ NULL
 .datatable.aware <- TRUE
 
 .onLoad <- function(libname, pkgname) {
-  ## Conflicts with chromatographR
-  # options("pboptions" = list(
-  #   type = if (interactive()) "timer" else "none",
-  #   char = "-",
-  #   txt.width = 50,
-  #   gui.width = 300,
-  #   style = 3,
-  #   initial = 0,
-  #   title = "R progress bar",
-  #   label = "",
-  #   nout = 100L
-  # ))
   strat <- ifelse(test = .Platform$OS.type == "unix",
     yes = "multicore",
     no = "multisession"
   )
-  future::plan(strategy = strat, workers = future::nbrOfWorkers())
-  progressr::handlers(
-    progressr::handler_txtprogressbar(enable = TRUE),
-    progressr::handler_progress(format = ":spin [:bar] ETA: :eta :percent")
-  )
+  future::plan(strategy = strat)
+  message("Running in ", strat, " with ", future::nbrOfWorkers(), " workers detected")
   invisible(NULL)
 }
 
