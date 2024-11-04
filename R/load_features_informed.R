a584ad81f88321fa14fa881796efdcc1e240cfc5
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-04 08:00:05 +0100

    refactor(data loading and processing): improve annotation and feature loading functions to reduce complexity and increase readability

---
+++ R/load_features_informed.R
@@ -0,0 +1,19 @@
+#' Load features informed
+#'
+#' @param file File
+#' @param show_example Show example? Default to FALSE
+#'
+#' @return A table of informed features
+#'
+#' @examples NULL
+load_features_informed <- function(file = NULL,
+                                   show_example = FALSE) {
+  if (show_example) {
+    # features_informed |>
+    #   saveRDS(file = "inst/extdata/features_informed.rds")
+    readRDS(system.file("extdata", "features_informed.rds", package = "cascade"))
+  } else {
+    file |>
+      tidytable::fread()
+  }
+}
