b39c8a9710b5e575dd69e0eca2f801166b3fa277
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-04 13:53:41 +0100

    native pipe

--- R/format_gt.R
+++ R/format_gt.R
@@ -8,27 +8,27 @@
 #'
 #' @examples NULL
 temp_gt_function <- function(table, title, subtitle) {
-  prettyTable <- table %>%
+  prettyTable <- table |>
     dplyr::mutate(
       structure = purrr::map(structure, ~ htmltools::a(href = .x, .x)),
       structure = purrr::map(structure, ~ gt::html(as.character(.x)))
-    ) %>%
+    ) |>
     dplyr::mutate(
       taxa = purrr::map(taxa, ~ htmltools::a(href = .x, .x)),
       taxa = purrr::map(taxa, ~ gt::html(as.character(.x)))
-    ) %>%
+    ) |>
     dplyr::mutate(
       references = purrr::map(references, ~ htmltools::a(href = .x, .x)),
       references = purrr::map(references, ~ gt::html(as.character(.x)))
-    ) %>%
-    gt::gt() %>%
+    ) |>
+    gt::gt() |>
     # cols_width(
     #   everything() ~ px(200)
-    # ) %>%
+    # ) |>
     gt::tab_header(
       title = gt::md(title),
       subtitle = gt::md(subtitle)
-    ) %>%
+    ) |>
     gt::text_transform(
       locations = gt::cells_body(columns = structureImage),
       fn = molinfo
