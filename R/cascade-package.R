57643688339882160d6940b4887de1db6bf4c532
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2024-11-04 16:58:13 +0100

    Update cascade-package.R

--- R/cascade-package.R
+++ R/cascade-package.R
@@ -7,15 +7,15 @@ NULL
 .datatable.aware <- TRUE
 
 .onLoad <- function(libname, pkgname) {
+  invisible(NULL)
+}
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to CASCADE")
   strat <- ifelse(test = .Platform$OS.type == "unix",
     yes = "multicore",
     no = "multisession"
   )
   future::plan(strategy = strat)
   message("Running in ", strat, " with ", future::nbrOfWorkers(), " workers detected")
-  invisible(NULL)
-}
-
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to CASCADE")
 }
