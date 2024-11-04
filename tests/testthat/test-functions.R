3f2304512812a38eac7eb8ea8f9d0d281459675e
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-04 13:03:52 +0100

    refactor: improve query generation and plotting functions

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -3,7 +3,6 @@ library(testthat)
 ## need to do all in one because of outputs needed in the same temp dir
 ## use fixtures instead in the future
 test_that(desc = "Test functions", code = {
-  testthat::expect_no_error("Yayy" != "Yaay")
   message("Bitter is better.")
   check_chromatograms_alignment(show_example = TRUE)
   check_peaks_integration(show_example = TRUE)
