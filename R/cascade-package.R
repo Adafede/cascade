9f28a91785f28ba764bfa9ab21b6b9b9e3a086b3
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-06 15:43:45 +0100

    refactor(code): replace future and furrr with equivalent functions from ggplot2 and dplyr packages

--- R/cascade-package.R
+++ R/cascade-package.R
@@ -7,22 +7,10 @@ NULL
 .datatable.aware <- TRUE
 
 .onLoad <- function(libname, pkgname) {
-  strat <- ifelse(test = .Platform$OS.type == "unix",
-    yes = "multicore",
-    no = "sequential"
-  )
-  future::plan(strategy = strat)
-  packageStartupMessage(
-    "Running in ",
-    strat,
-    " with ",
-    future::nbrOfWorkers(),
-    " workers detected"
-  )
   invisible(NULL)
 }
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to  ", pkgname)
+  packageStartupMessage("Welcome to ", pkgname)
   message(format(utils::citation(pkgname)))
 }
--- R/treemaps_progress.R
+++ R/treemaps_progress.R
@@ -11,33 +11,13 @@ treemaps_progress <- function(xs,
                               type = "treemap",
                               hierarchies) {
   p <- progressr::progressor(along = xs)
-  future.apply::future_lapply(
-    X = setNames(object = xs, nm = xs),
-    FUN = function(x, hierarchies) {
-      p()
-      if (x != "special") {
-        plotly::plot_ly(
-          data = hierarchies[[x]],
-          ids = ~ids,
-          labels = ~labels,
-          parents = ~parents,
-          values = ~values,
-          maxdepth = 3,
-          type = type,
-          branchvalues = "total",
-          textinfo = "label+percent value+percent parent+percent root"
-        ) |>
-          plotly::layout(
-            colorway = microshades_colors,
-            title = paste(x, "(", nrow(
-              tables[[x]] |> tidytable::distinct(structure)
-            ), ")"),
-            margin = list(t = 40)
-          )
-      } else {
-        plotly::plot_ly() |>
-          plotly::add_trace(
-            data = hierarchies[[unique(hierarchies[[x]]$species)[1]]],
+  setNames(object = xs, nm = xs) |>
+    furrr::future_map(
+      .f = function(x, hierarchies) {
+        p()
+        if (x != "special") {
+          plotly::plot_ly(
+            data = hierarchies[[x]],
             ids = ~ids,
             labels = ~labels,
             parents = ~parents,
@@ -45,45 +25,65 @@ treemaps_progress <- function(xs,
             maxdepth = 3,
             type = type,
             branchvalues = "total",
-            textinfo = "label+percent value+percent parent+percent root",
-            domain = list(row = 0, column = 0)
+            textinfo = "label+percent value+percent parent+percent root"
           ) |>
-          plotly::add_trace(
-            data = hierarchies[[unique(hierarchies[[x]]$species)[2]]],
-            ids = ~ids,
-            labels = ~labels,
-            parents = ~parents,
-            values = ~values,
-            maxdepth = 3,
-            type = type,
-            branchvalues = "total",
-            textinfo = "label+percent value+percent parent+percent root",
-            domain = list(row = 0, column = 1)
-          ) |>
-          plotly::layout(
-            title = paste(
-              "Comparative analysis",
-              "\n",
-              unique(hierarchies[[x]]$species)[1],
-              "(",
-              nrow(tables[[unique(hierarchies[[x]]$species)[1]]] |>
-                tidytable::distinct(structure)),
-              ")",
-              "                                 ",
-              unique(hierarchies[[x]]$species)[2],
-              "(",
-              nrow(tables[[unique(hierarchies[[x]]$species)[2]]] |>
-                tidytable::distinct(structure)),
-              ")"
-            ),
-            grid = list(rows = 1, columns = 2),
-            colorway = microshades_colors,
-            margin = list(t = 40)
-          )
-      }
-    },
-    hierarchies = hierarchies
-  )
+            plotly::layout(
+              colorway = microshades_colors,
+              title = paste(x, "(", nrow(
+                tables[[x]] |> tidytable::distinct(structure)
+              ), ")"),
+              margin = list(t = 40)
+            )
+        } else {
+          plotly::plot_ly() |>
+            plotly::add_trace(
+              data = hierarchies[[unique(hierarchies[[x]]$species)[1]]],
+              ids = ~ids,
+              labels = ~labels,
+              parents = ~parents,
+              values = ~values,
+              maxdepth = 3,
+              type = type,
+              branchvalues = "total",
+              textinfo = "label+percent value+percent parent+percent root",
+              domain = list(row = 0, column = 0)
+            ) |>
+            plotly::add_trace(
+              data = hierarchies[[unique(hierarchies[[x]]$species)[2]]],
+              ids = ~ids,
+              labels = ~labels,
+              parents = ~parents,
+              values = ~values,
+              maxdepth = 3,
+              type = type,
+              branchvalues = "total",
+              textinfo = "label+percent value+percent parent+percent root",
+              domain = list(row = 0, column = 1)
+            ) |>
+            plotly::layout(
+              title = paste(
+                "Comparative analysis",
+                "\n",
+                unique(hierarchies[[x]]$species)[1],
+                "(",
+                nrow(tables[[unique(hierarchies[[x]]$species)[1]]] |>
+                  tidytable::distinct(structure)),
+                ")",
+                "                                 ",
+                unique(hierarchies[[x]]$species)[2],
+                "(",
+                nrow(tables[[unique(hierarchies[[x]]$species)[2]]] |>
+                  tidytable::distinct(structure)),
+                ")"
+              ),
+              grid = list(rows = 1, columns = 2),
+              colorway = microshades_colors,
+              margin = list(t = 40)
+            )
+        }
+      },
+      hierarchies = hierarchies
+    )
 }
 
 #' Treemaps progress no title
@@ -98,26 +98,14 @@ treemaps_progress <- function(xs,
 treemaps_progress_no_title <- function(xs,
                                        type = "treemap",
                                        hierarchies) {
-  future.apply::future_lapply(
-    X = setNames(object = xs, nm = xs),
-    FUN = function(x, hierarchies) {
-      if (x != "special") {
-        plotly::plot_ly(
-          data = hierarchies[[x]],
-          ids = ~ids,
-          labels = ~labels,
-          parents = ~parents,
-          values = ~values,
-          maxdepth = 3,
-          type = type,
-          branchvalues = "total",
-          textinfo = "label+percent value+percent parent+percent root"
-        ) |>
-          plotly::layout(colorway = microshades_colors, margin = list(t = 40))
-      } else {
-        plotly::plot_ly() |>
-          plotly::add_trace(
-            data = hierarchies[[unique(hierarchies[[x]]$species)[1]]],
+  p <- progressr::progressor(along = xs)
+  setNames(object = xs, nm = xs) |>
+    furrr::future_map(
+      .f = function(x, hierarchies) {
+        p()
+        if (x != "special") {
+          plotly::plot_ly(
+            data = hierarchies[[x]],
             ids = ~ids,
             labels = ~labels,
             parents = ~parents,
@@ -125,28 +113,42 @@ treemaps_progress_no_title <- function(xs,
             maxdepth = 3,
             type = type,
             branchvalues = "total",
-            textinfo = "label+percent value+percent parent+percent root",
-            domain = list(row = 0, column = 0)
+            textinfo = "label+percent value+percent parent+percent root"
           ) |>
-          plotly::add_trace(
-            data = hierarchies[[unique(hierarchies[[x]]$species)[2]]],
-            ids = ~ids,
-            labels = ~labels,
-            parents = ~parents,
-            values = ~values,
-            maxdepth = 3,
-            type = type,
-            branchvalues = "total",
-            textinfo = "label+percent value+percent parent+percent root",
-            domain = list(row = 0, column = 1)
-          ) |>
-          plotly::layout(
-            grid = list(rows = 1, columns = 2),
-            colorway = microshades_colors,
-            margin = list(t = 40)
-          )
-      }
-    },
-    hierarchies = hierarchies
-  )
+            plotly::layout(colorway = microshades_colors, margin = list(t = 40))
+        } else {
+          plotly::plot_ly() |>
+            plotly::add_trace(
+              data = hierarchies[[unique(hierarchies[[x]]$species)[1]]],
+              ids = ~ids,
+              labels = ~labels,
+              parents = ~parents,
+              values = ~values,
+              maxdepth = 3,
+              type = type,
+              branchvalues = "total",
+              textinfo = "label+percent value+percent parent+percent root",
+              domain = list(row = 0, column = 0)
+            ) |>
+            plotly::add_trace(
+              data = hierarchies[[unique(hierarchies[[x]]$species)[2]]],
+              ids = ~ids,
+              labels = ~labels,
+              parents = ~parents,
+              values = ~values,
+              maxdepth = 3,
+              type = type,
+              branchvalues = "total",
+              textinfo = "label+percent value+percent parent+percent root",
+              domain = list(row = 0, column = 1)
+            ) |>
+            plotly::layout(
+              grid = list(rows = 1, columns = 2),
+              colorway = microshades_colors,
+              margin = list(t = 40)
+            )
+        }
+      },
+      hierarchies = hierarchies
+    )
 }
